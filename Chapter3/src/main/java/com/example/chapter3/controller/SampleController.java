dca05b23cf7273d9b8081b77600cab71c47693f3
DevJaepaL <104006620+DevJaepaL@users.noreply.github.com>
2023-04-18 15:19:52 +0900

    [챕터 3 : 사이드 바 파라미터 테스트]

--- Chapter3/src/main/java/com/example/chapter3/controller/SampleController.java
+++ Chapter3/src/main/java/com/example/chapter3/controller/SampleController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -35,5 +36,10 @@ public class SampleController {
                 }).collect(Collectors.toList());
 
         model.addAttribute("list" , list);
+    }
+
+    @GetMapping({"/exLayout1", "/exLayout2", "/exTemplate", "/exSidebar"})
+    public void exLayout1() {
+        log.info("ex Layout . . . .");
     }
 }
