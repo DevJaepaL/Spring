033ba564749420d89a1c287b2214cbe813adc5b8
DevJaepaL <104006620+DevJaepaL@users.noreply.github.com>
2023-06-13 14:26:29 +0900

    [GuestbookServiceTests] 패키지 이동

---
+++ GuestBook/src/test/java/kr/ac/kopo/guestbook/service/GustbookServiceTests.java
@@ -0,0 +1,67 @@
+package kr.ac.kopo.guestbook.service;
+
+import kr.ac.kopo.guestbook.dto.GuestbookDTO;
+import kr.ac.kopo.guestbook.dto.PageRequestDTO;
+import kr.ac.kopo.guestbook.dto.PageResultDTO;
+import kr.ac.kopo.guestbook.entity.Guestbook;
+import kr.ac.kopo.guestbook.service.GuestbookService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class GustbookServiceTests {
+    @Autowired
+    private GuestbookService service;
+
+    @Test
+    public void testRegister(){
+        GuestbookDTO guestbookDTO = GuestbookDTO.builder()
+                .title("Sample Title...")
+                .content("Sample Content...")
+                .writer("userSample")
+                .build();
+
+        System.out.println(service.register(guestbookDTO));
+    }
+    @Test
+    public void testList(){
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder()
+                .page(1)
+                .size(10)
+                .build();
+        PageResultDTO<GuestbookDTO, Guestbook> resultDTO = service.getList(pageRequestDTO);
+        System.out.println("prev: " + resultDTO.isPrev());
+        System.out.println("next: " + resultDTO.isNext());
+        System.out.println("total: " + resultDTO.getTotalPage());
+        System.out.println("==========================================");
+        for (GuestbookDTO guestbookDTO : resultDTO.getDtoList()){
+            System.out.println(guestbookDTO);
+        }
+
+        resultDTO.getPageList().forEach(i -> System.out.println(i));
+
+    }
+
+    @Test
+    public void testSearch() {
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder()
+                .page(1)
+                .size(10)
+                .type("tcw") // 검색 조건 : t, c, w, tc . . . 등등 tc는 title & contents(제목 및 글 내용)
+                .keyword("Title")
+                .build();
+
+        PageResultDTO<GuestbookDTO, Guestbook> resultDTO = service.getList(pageRequestDTO);
+
+        System.out.println("이전 : " + resultDTO.isPrev());
+        System.out.println("다음 : " + resultDTO.isNext());
+        System.out.println("총합 : " + resultDTO.getTotalPage());
+        System.out.println("===========================================");
+        for(GuestbookDTO guestbookDTO : resultDTO.getDtoList()) {
+            System.out.println(guestbookDTO);
+        }
+        System.out.println("===========================================");
+        resultDTO.getPageList().forEach(i -> System.out.println(i));
+    }
+}
