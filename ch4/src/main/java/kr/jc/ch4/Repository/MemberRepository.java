641d7d2296854f7324669f41c11839560702c5b9
JaepaL <104006620+DevJaepaL@users.noreply.github.com>
2023-10-31 13:46:24 +0900

    :wastebasket: Ch.4 Project 삭제
    버전 호환 에러 & 새로운 프로젝트 생성을 위한 삭제

--- ch4/src/main/java/kr/jc/ch4/Repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package kr.jc.ch4.Repository;
-
-import kr.jc.ch4.Entity.CommunityMember;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<CommunityMember, String> {
-}
--- ch4/src/main/java/kr/jc/ch4/Repository/UserBoardRepository.java
+++
@@ -1,7 +0,0 @@
-package kr.jc.ch4.Repository;
-
-import kr.jc.ch4.Entity.UserBoard;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserBoardRepository extends JpaRepository<UserBoard, Long> {
-}
