500adbccf0dea135d5a6e13cdf5835d7f4cd0ca7
JaepaL <104006620+DevJaepaL@users.noreply.github.com>
2023-10-31 14:39:01 +0900

    :heavy_plus_sign: Add File

--- Chapter5/src/main/java/kr/jc/chapter5/Chapter5Application.java
+++ Chapter5/src/main/java/kr/jc/chapter5/Chapter5Application.java
@@ -2,8 +2,10 @@ package kr.jc.chapter5;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class Chapter5Application {
 
 	public static void main(String[] args) {
