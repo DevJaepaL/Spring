00b6bca94cf8ad0651ca66850fd052eb1c54e951
JaepaL <104006620+DevJaepaL@users.noreply.github.com>
2023-11-01 11:00:02 +0900

    :fire: Remove Project

--- Chapter5/src/main/java/kr/jc/chapter5/Repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package kr.jc.chapter5.Repository;
-
-import kr.jc.chapter5.Entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, String> {
-}
--- Chapter5/src/main/java/kr/jc/chapter5/Repository/ReplyRepository.java
+++
@@ -1,7 +0,0 @@
-package kr.jc.chapter5.Repository;
-
-import kr.jc.chapter5.Entity.Reply;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReplyRepository extends JpaRepository<Reply, Long> {
-}
