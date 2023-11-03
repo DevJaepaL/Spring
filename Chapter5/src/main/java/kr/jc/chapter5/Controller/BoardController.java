bed08465adc70a2f7b99daa823a83f61dc2408ec
JaepaL <104006620+DevJaepaL@users.noreply.github.com>
2023-11-03 13:17:19 +0900

    :sparkles: 컨트롤러 추가

--- Chapter5/src/main/java/kr/jc/chapter5/Controller/BoardController.java
+++ Chapter5/src/main/java/kr/jc/chapter5/Controller/BoardController.java
@@ -1,9 +1,12 @@
 package kr.jc.chapter5.Controller;
 
+import kr.jc.chapter5.DTO.PageRequestDTO;
 import kr.jc.chapter5.ServiceTest.BoardService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,4 +15,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 public class BoardController {
     private final BoardService boardService;
+
+    @GetMapping("/list")
+    public void list(PageRequestDTO pageRequestDTO, Model model) {
+        log.info("--------- List ---------");
+        model.addAttribute("result", boardService.getList(pageRequestDTO));
+    }
 }
